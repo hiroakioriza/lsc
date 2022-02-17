dd8d00f29dbb264c9b4e715234909dcea94d24f5
Flabib <fahdilabib@gmail.com>
2022-02-17 08:49:11 +0700

    Add ServisanSeeder and UserSeeder Fixing Responsive

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // Call another seeder
+        $this->call([
+            UserSeeder::class,
+            ServisanSeeder::class,
+        ]);
     }
 }
--- resources/views/layoutdash.blade.php
+++ resources/views/layoutdash.blade.php
@@ -2,6 +2,8 @@
 <html data-theme="cupcake">
 <head>
     <title>LSC Site</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="icon" href="{{ url('img/favicon.ico') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
